35cd476f69729a6133f0afa77315c8062cef288f
KKardd <wlstjd4013@gmail.com>
2023-04-18 17:09:53 +0900

    feat: apply mongodb to auth

--- Server/data/auth.js
+++ Server/data/auth.js
@@ -1,52 +1,74 @@
-//abcd1234: $2a$12$lNMCgsXnkG35KrLoxW49qO3DOR2WTufyLDZ5zNiy.0Xqclv25YA8S
-//mac1234:$2a$12$o0fEOgaNkPL/YOXPW7aaS.h9yFGKUsLZDS.dYQ1kToU9lhaX6tUri
-import {db, sequelize} from "../db/database.js";
-import SQ, {TEXT} from "sequelize";
-const DateTypes = SQ.DataTypes;
-
-export const User = sequelize.define(
-    "user",
-    {
-        id: {
-            type: DateTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true,
-        },
-        username: {
-            type: DateTypes.STRING(128),
-            allowNull: false,
-        },
-        password: {
-            type: DateTypes.STRING(128),
-            allowNull: false,
-        },
-        name: {
-            type: DateTypes.STRING(128),
-            allowNull: false,
-        },
-        email: {
-            type: DateTypes.STRING(128),
-            allowNull: false,
-        },
-        url: {
-            type: DateTypes.TEXT,
-        },
-    },
-    {timestamps: false}
-);
+// import {db, getUsers, sequelize} from "../db/database.js";
+import {getUsers} from "../db/database.js";
+import MongoDb from "mongodb";
+const objectId = MongoDb.ObjectId;
 
 export async function findByUsername(username) {
-    return User.findOne({where: {username: username}});
+    return getUsers().findOne({username: username}).then(mapOptionalUser);
 }
 
 export async function findById(id) {
-    return User.findByPk(id);
+    return getUsers()
+        .findOne({_id: new objectId(id)})
+        .then(mapOptionalUser);
 }
 
 export async function createUser(user) {
-    return User.create(user).then((result) => result.dataValues.id);
+    return getUsers()
+        .insertOne(user)
+        .then((data) => data.insertedId.toString());
 }
+
+function mapOptionalUser(user) {
+    return user ? {...user, id: user._id} : user;
+}
+
+// import SQ, {TEXT} from "sequelize";
+// const DateTypes = SQ.DataTypes;
+
+// export const User = sequelize.define(
+//     "user",
+//     {
+//         id: {
+//             type: DateTypes.INTEGER,
+//             autoIncrement: true,
+//             allowNull: false,
+//             primaryKey: true,
+//         },
+//         username: {
+//             type: DateTypes.STRING(128),
+//             allowNull: false,
+//         },
+//         password: {
+//             type: DateTypes.STRING(128),
+//             allowNull: false,
+//         },
+//         name: {
+//             type: DateTypes.STRING(128),
+//             allowNull: false,
+//         },
+//         email: {
+//             type: DateTypes.STRING(128),
+//             allowNull: false,
+//         },
+//         url: {
+//             type: DateTypes.TEXT,
+//         },
+//     },
+//     {timestamps: false}
+// );
+
+// export async function findByUsername(username) {
+//     return User.findOne({where: {username: username}});
+// }
+
+// export async function findById(id) {
+//     return User.findByPk(id);
+// }
+
+// export async function createUser(user) {
+//     return User.create(user).then((result) => result.dataValues.id);
+// }
 
 // mysql
 
