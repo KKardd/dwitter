125d2b041b75e9bfe07bac8714c5b0493b25a46b
KKardd <wlstjd4013@gmail.com>
2023-04-19 17:46:20 +0900

    feat: receive tweet for mongodb

--- Server/data/tweet.js
+++ Server/data/tweet.js
@@ -1,117 +1,175 @@
-import SQ, {TEXT} from "sequelize";
-import {User} from "./auth.js";
-import {db, sequelize} from "../db/database.js";
-
-const Sequelize = SQ.Sequelize;
-const DateTypes = SQ.DataTypes;
-
-const Tweet = sequelize.define("tweet", {
-    id: {
-        type: DateTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    text: {
-        type: DateTypes.TEXT,
-        allowNull: false,
-    },
-});
-Tweet.belongsTo(User);
-
-const INCLUDED_USER = {
-    attributes: [
-        "id",
-        "text",
-        "createdAt",
-        "userId",
-        [Sequelize.col("user.name"), "name"],
-        [Sequelize.col("user.username"), "username"],
-        [Sequelize.col("user.url"), "url"],
-    ],
-    include: {
-        model: User,
-        attributes: [],
-    },
-};
-const ORDER_DESC = {order: [["createdAt", "DESC"]]};
+// import SQ, {TEXT} from "sequelize";
+// import {User} from "./auth.js";
+// import {db, sequelize} from "../db/database.js";
+import MongoDb, {ObjectId} from "mongodb";
+import {getTweets} from "../db/database.js";
+import {findById} from "./auth.js";
+const objectId = MongoDb.ObjectId;
 
 export async function getAll() {
-    return Tweet.findAll({...INCLUDED_USER, ...ORDER_DESC});
+    return getTweets().find().sort({createdAt: -1}).toArray().then(mapTweets);
 }
 
 export async function getAllByUsername(username) {
-    return Tweet.findAll({
-        ...INCLUDED_USER,
-        ...ORDER_DESC,
-        include: {
-            ...INCLUDED_USER.include,
-            where: {username},
-        },
-    });
+    return getTweets().find({username}).sort({createdAt: -1}).toArray().then(mapTweets);
 }
 
 export async function getById(id) {
-    return Tweet.findOne({
-        where: {id},
-        ...INCLUDED_USER,
-    });
+    return getTweets()
+        .findOne({_id: new objectId(id)})
+        .then(mapOptionalTweet);
 }
 
 export async function create(text, userId) {
-    return Tweet.create({text, userId}).then((result) => getById(result.dataValues.id));
+    const {name, username, url} = await findById(userId);
+    const tweet = {
+        text,
+        createdAt: new Date(),
+        userId,
+        name,
+        username,
+        url,
+    };
+    console.log(tweet);
+    return getTweets()
+        .insertOne(tweet)
+        .then((data) => {
+            return mapOptionalTweet({...tweet, _id: data.insertedId});
+        });
 }
 
 export async function update(id, text) {
-    return Tweet.findByPk(id, {
-        ...INCLUDED_USER,
-    }).then((tweet) => {
-        tweet.text = text;
-        return tweet.save();
-    });
+    return getTweets()
+        .findOneAndUpdate(
+            {_id: new objectId(id)}, //
+            {$set: {text}},
+            {returnDocument: "after"}
+        )
+        .then((result) => result.value)
+        .then(mapOptionalTweet);
 }
 
 export async function remove(id) {
-    return Tweet.findByPk(id, {
-        ...INCLUDED_USER,
-    }).then((tweet) => {
-        tweet.destroy();
-    });
+    return getTweets().deleteOne({_id: new objectId(id)});
 }
 
-// mysql
+function mapOptionalTweet(tweet) {
+    return tweet ? {...tweet, id: tweet._id.toString()} : tweet;
+}
 
-// const SELECT_JOIN =
-//     "SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.name, us.username, us.url FROM tweets as tw JOIN users as us ON tw.userId = us.id";
-// const ORDER_DESC = "ORDER BY tw.createdAt DESC";
+function mapTweets(tweet) {
+    return tweet.map(mapOptionalTweet);
+}
+// const Sequelize = SQ.Sequelize;
+// const DateTypes = SQ.DataTypes;
+
+// const Tweet = sequelize.define("tweet", {
+//     id: {
+//         type: DateTypes.INTEGER,
+//         autoIncrement: true,
+//         allowNull: false,
+//         primaryKey: true,
+//     },
+//     text: {
+//         type: DateTypes.TEXT,
+//         allowNull: false,
+//     },
+// });
+// Tweet.belongsTo(User);
+
+// const INCLUDED_USER = {
+//     attributes: [
+//         "id",
+//         "text",
+//         "createdAt",
+//         "userId",
+//         [Sequelize.col("user.name"), "name"],
+//         [Sequelize.col("user.username"), "username"],
+//         [Sequelize.col("user.url"), "url"],
+//     ],
+//     include: {
+//         model: User,
+//         attributes: [],
+//     },
+// };
+// const ORDER_DESC = {order: [["createdAt", "DESC"]]};
+
 // export async function getAll() {
-//     return db
-//         .execute(`${SELECT_JOIN} ${ORDER_DESC}`) //
-//         .then((result) => result[0]);
+//     return Tweet.findAll({...INCLUDED_USER, ...ORDER_DESC});
 // }
 
 // export async function getAllByUsername(username) {
-//     return db
-//         .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username]) //
-//         .then((result) => result[0]);
+//     return Tweet.findAll({
+//         ...INCLUDED_USER,
+//         ...ORDER_DESC,
+//         include: {
+//             ...INCLUDED_USER.include,
+//             where: {username},
+//         },
+//     });
 // }
 
 // export async function getById(id) {
-//     return db
-//         .execute(`${SELECT_JOIN} WHERE tw.id=?`, [id]) //
-//         .then((result) => result[0][0]);
+//     return Tweet.findOne({
+//         where: {id},
+//         ...INCLUDED_USER,
+//     });
 // }
 
 // export async function create(text, userId) {
-//     return db
-//         .execute("INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)", [text, new Date(), userId])
-//         .then((result) => getById(result[0].insertId));
+//     return Tweet.create({text, userId}).then((result) => getById(result.dataValues.id));
 // }
 
 // export async function update(id, text) {
-//     return db.execute("UPDATE tweets SET text=? WHERE id=?", [text, id]).then(() => getById(id));
+//     return Tweet.findByPk(id, {
+//         ...INCLUDED_USER,
+//     }).then((tweet) => {
+//         tweet.text = text;
+//         return tweet.save();
+//     });
 // }
 
 // export async function remove(id) {
-//     return db.execute("DELETE FROM tweets WHERE id=?", [id]);
+//     return Tweet.findByPk(id, {
+//         ...INCLUDED_USER,
+//     }).then((tweet) => {
+//         tweet.destroy();
+//     });
 // }
+
+// // mysql
+
+// // const SELECT_JOIN =
+// //     "SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.name, us.username, us.url FROM tweets as tw JOIN users as us ON tw.userId = us.id";
+// // const ORDER_DESC = "ORDER BY tw.createdAt DESC";
+// // export async function getAll() {
+// //     return db
+// //         .execute(`${SELECT_JOIN} ${ORDER_DESC}`) //
+// //         .then((result) => result[0]);
+// // }
+
+// // export async function getAllByUsername(username) {
+// //     return db
+// //         .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username]) //
+// //         .then((result) => result[0]);
+// // }
+
+// // export async function getById(id) {
+// //     return db
+// //         .execute(`${SELECT_JOIN} WHERE tw.id=?`, [id]) //
+// //         .then((result) => result[0][0]);
+// // }
+
+// // export async function create(text, userId) {
+// //     return db
+// //         .execute("INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)", [text, new Date(), userId])
+// //         .then((result) => getById(result[0].insertId));
+// // }
+
+// // export async function update(id, text) {
+// //     return db.execute("UPDATE tweets SET text=? WHERE id=?", [text, id]).then(() => getById(id));
+// // }
+
+// // export async function remove(id) {
+// //     return db.execute("DELETE FROM tweets WHERE id=?", [id]);
+// // }
