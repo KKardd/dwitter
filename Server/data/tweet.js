c5ea74b66ec64140cf0f26141a7b1c86ba5ed4e6
KKardd <wlstjd4013@gmail.com>
2023-04-20 00:43:15 +0900

    style: formating by database type

--- Server/data/tweet.js
+++ Server/data/tweet.js
@@ -4,6 +4,10 @@
 import MongoDb, {ObjectId} from "mongodb";
 import {getTweets} from "../db/database.js";
 import {findById} from "./auth.js";
+
+// -----------------------------------------------------------
+
+// mongodb 사용 시
 const objectId = MongoDb.ObjectId;
 
 export async function getAll() {
@@ -59,6 +63,9 @@ function mapOptionalTweet(tweet) {
 function mapTweets(tweet) {
     return tweet.map(mapOptionalTweet);
 }
+// -----------------------------------------------------------
+
+// Sequelize 사용 시
 
 // const Sequelize = SQ.Sequelize;
 // const DateTypes = SQ.DataTypes;
@@ -136,40 +143,41 @@ function mapTweets(tweet) {
 //         tweet.destroy();
 //     });
 // }
+// -----------------------------------------------------------
 
-// // mysql
+// mysql 사용 시
 
-// // const SELECT_JOIN =
-// //     "SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.name, us.username, us.url FROM tweets as tw JOIN users as us ON tw.userId = us.id";
-// // const ORDER_DESC = "ORDER BY tw.createdAt DESC";
-// // export async function getAll() {
-// //     return db
-// //         .execute(`${SELECT_JOIN} ${ORDER_DESC}`) //
-// //         .then((result) => result[0]);
-// // }
+// const SELECT_JOIN =
+//     "SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.name, us.username, us.url FROM tweets as tw JOIN users as us ON tw.userId = us.id";
+// const ORDER_DESC = "ORDER BY tw.createdAt DESC";
+// export async function getAll() {
+//     return db
+//         .execute(`${SELECT_JOIN} ${ORDER_DESC}`) //
+//         .then((result) => result[0]);
+// }
 
-// // export async function getAllByUsername(username) {
-// //     return db
-// //         .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username]) //
-// //         .then((result) => result[0]);
-// // }
+// export async function getAllByUsername(username) {
+//     return db
+//         .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username]) //
+//         .then((result) => result[0]);
+// }
 
-// // export async function getById(id) {
-// //     return db
-// //         .execute(`${SELECT_JOIN} WHERE tw.id=?`, [id]) //
-// //         .then((result) => result[0][0]);
-// // }
+// export async function getById(id) {
+//     return db
+//         .execute(`${SELECT_JOIN} WHERE tw.id=?`, [id]) //
+//         .then((result) => result[0][0]);
+// }
 
-// // export async function create(text, userId) {
-// //     return db
-// //         .execute("INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)", [text, new Date(), userId])
-// //         .then((result) => getById(result[0].insertId));
-// // }
+// export async function create(text, userId) {
+//     return db
+//         .execute("INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)", [text, new Date(), userId])
+//         .then((result) => getById(result[0].insertId));
+// }
 
-// // export async function update(id, text) {
-// //     return db.execute("UPDATE tweets SET text=? WHERE id=?", [text, id]).then(() => getById(id));
-// // }
+// export async function update(id, text) {
+//     return db.execute("UPDATE tweets SET text=? WHERE id=?", [text, id]).then(() => getById(id));
+// }
 
-// // export async function remove(id) {
-// //     return db.execute("DELETE FROM tweets WHERE id=?", [id]);
-// // }
+// export async function remove(id) {
+//     return db.execute("DELETE FROM tweets WHERE id=?", [id]);
+// }
